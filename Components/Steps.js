8997d99746c5699a200695855b073faba1e933bb
Lydia Kwa <lydiakwa@gmail.com>
2022-07-04 14:11:53 -0400

    healthkit setup

---
+++ Components/Steps.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { useStepCount } from '../Healthkit';
+
+const Steps = () => {
+  const steps = useStepCount();
+  return (
+    <View style={styles.container}>
+      <Text>Steps: {steps}</Text>
+      <Text>Test</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Steps;
