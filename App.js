0b23de50aacabde67c932bc38ab6bf19714305bf
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-07-04 17:06:02 -0400

    chore: remove debugger

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import { HealthKitProvider } from './Healthkit';
 import Steps from './Components/Steps';
 
