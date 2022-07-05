bd65e4537b4e40d1aa313cf641315927c0e87907
Lydia Kwa <lydiakwa@gmail.com>
2022-07-05 17:29:10 -0400

    fix: missing dependency in navigation run npx react-native run-ios to do a rebuild

--- src/App.js
+++ src/App.js
@@ -15,9 +15,14 @@ export default function App() {
       <HealthKitProvider>
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ title: 'Home' }}
+            />
           </Stack.Navigator>
         </NavigationContainer>
+        <Home />
       </HealthKitProvider>
     </QueryClientProvider>
   );
