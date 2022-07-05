3a797f27859332723d73d36be465c00479f41c0b
Kristin Lam <klam623@gmail.com>
2022-07-05 16:54:04 -0400

    feat: create User model

---
+++ server/db/models/User.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const User = db.define('user', {
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    },
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  carrotCount: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    min: 0,
+  },
+  dailyStepGoal: {
+    type: Sequelize.INTEGER,
+    defaultValue: 5000,
+    min: 0,
+  },
+});
+
+module.exports = User;
