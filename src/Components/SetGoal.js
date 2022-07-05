4c05ff00eac004e1449336cc14b9cbe7c3e971a4
Lydia Kwa <lydiakwa@gmail.com>
2022-07-05 16:32:37 -0400

    feat: create a setGoal react component

---
+++ src/Components/SetGoal.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SetGoal = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default SetGoal;
