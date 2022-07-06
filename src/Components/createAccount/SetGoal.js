211ae136226788673cbe9954d2d688ec59da835e
Lydia Kwa <lydiakwa@gmail.com>
2022-07-06 09:35:20 -0400

    feat: install react native paper may need to reinstall and rebuild app install also includes react-native-vector-icons

--- src/Components/createAccount/SetGoal.js
+++ src/Components/createAccount/SetGoal.js
@@ -3,11 +3,20 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 const SetGoal = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Select Your Daily Step Goal</Text>
       <TextInput placeholder="Step Goal" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default SetGoal;
