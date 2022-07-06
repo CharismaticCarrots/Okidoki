ec381cec5bfcd5467b4e44116dd822e6f4c086ab
Lydia Kwa <lydiakwa@gmail.com>
2022-07-06 14:55:59 -0400

    feat: create health stat react component

---
+++ src/Components/HealthStat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import Steps from './Steps';
+
+const HealthStat = () => {
+  return (
+    <View>
+      <Text>Health Stats</Text>
+    </View>
+  );
+};
+
+export default HealthStat;
