80a455e03eb34075371ebecd589e2412ce9e9f9c
Kristin Lam <klam623@gmail.com>
2022-07-06 16:15:19 -0400

    refactor: remove default port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { db } = require('./db');
 const app = require('./app');
-const port = process.env.PORT || 19002;
+const port = process.env.PORT;
 
 db.sync().then(function () {
   app.listen(port, function () {
