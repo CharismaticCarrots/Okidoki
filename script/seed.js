0383384e09558fb4dac0efe4cf067cd336f9d740
Connie Chan <conniec321@gmail.com>
2022-07-06 13:17:46 -0400

    chore: seed associations/#85

--- script/seed.js
+++ script/seed.js
@@ -1,8 +1,11 @@
-const { db, Doki } = require('../server/db/');
+const { db, Doki, User } = require('../server/db/');
 
 const dokis = [
   {
     type: 'cat',
+    spriteSheet: {
+      idle: '../../assets/catSprites/cat.png'
+    }
   },
   {
     type: 'bunny',
@@ -12,6 +15,35 @@ const dokis = [
   },
 ];
 
+const users = [
+  {
+    email: 'hello@gmail.com',
+    password: 'password',
+    firstName: 'Con',
+    lastName: 'Man',
+  },
+  {
+    email: 'bye@gmail.com',
+    password: 'password',
+    firstName: 'Angel',
+    lastName: 'Y',
+  },
+  {
+    email: 'hey@gmail.com',
+    password: 'password',
+    firstName: 'Kris',
+    lastName: 'Tin',
+  },
+  {
+    email: 'test@gmail.com',
+    password: 'password',
+    firstName: 'Ly',
+    lastName: 'Diaaa',
+  },
+]
+
+
+
 const seed = async () => {
   try {
     await db.sync({ force: true });
@@ -20,6 +52,16 @@ const seed = async () => {
         return Doki.create(doki);
       })
     );
+    await Promise.all(
+      users.map((user) => {
+        return User.create(user);
+      })
+    );
+    const Doki1 = await Doki.findByPk(1)
+    await Doki1.addUser(1, {through: {dokiName: 'Conbot'}})
+    await Doki1.addUser(2, {through: {dokiName: 'Snow Angel'}})
+    await Doki1.addUser(3, {through: {dokiName: 'Kris Kross'}})
+    await Doki1.addUser(4, {through: {dokiName: 'Ldyster'}})
   } catch (error) {
     console.log(error);
   }
