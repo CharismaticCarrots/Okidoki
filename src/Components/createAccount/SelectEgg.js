fefb4b0dfe852b3750cb7bf7c57b09f4f84cff70
Connie Chan <conniec321@gmail.com>
2022-07-06 16:40:44 -0400

    feat: select egg component/#32

--- src/Components/createAccount/SelectEgg.js
+++ src/Components/createAccount/SelectEgg.js
@@ -1,14 +1,59 @@
 import { StyleSheet, Text, View, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyledDokiHomeBackground } from '../styles'
+import Sprite from '../Sprite'
+import {Animated} from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
+import { Button } from 'react-native-paper';
 
 
 const SelectEgg = () => {
+  const [egg, setEgg] = useState(null)
   return (
-    <View>
+    <View >
       <StyledDokiHomeBackground source={require("../../../assets/selectEgg.png")} resizeMode="cover">
+      <View style={styles.container}>
       <Text style={styles.text}>Select a Doki Egg</Text>
       <TextInput placeholder="Doki Name"  style={styles.input}/>
+      <View style={styles.eggs}>
+      <Button
+      onPress={() => setEgg('egg1')}
+      >
+      <Sprite
+        src={require('../../../assets/greenEgg.png')}
+        totalSprites={egg == 'egg1' ? 1 :2}
+        tile={{ width: 600, height: 719 }}
+        scale={0.1}
+        framesPerSprite={29}
+      />
+      </Button>
+      <Button
+      onPress={() => setEgg('egg2')}
+      >
+      <Sprite
+        src={require('../../../assets/greenEgg.png')}
+        totalSprites={egg == 'egg2' ? 1 :2}
+        tile={{ width: 600, height: 719 }}
+        scale={0.1}
+        framesPerSprite={29}
+      />
+      </Button>
+      <Button
+      onPress={() => setEgg('egg3')}
+      >
+      <Sprite
+        src={require('../../../assets/greenEgg.png')}
+        totalSprites={egg == 'egg3' ? 1 :2}
+        tile={{ width: 600, height: 719 }}
+        scale={0.1}
+        framesPerSprite={29}
+      />
+      </Button>
+      </View>
+      <Button mode='contained'>
+        SUBMIT
+      </Button>
+      </View>
       </StyledDokiHomeBackground>
     </View>
   )
@@ -18,9 +63,23 @@ export default SelectEgg
 
 const styles = StyleSheet.create({
   input: {
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
+    height: 40,
+    width: 200
   },
   text: {
     fontSize: 40
+  },
+  eggs: {
+    flex: 0.35,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginTop: 140
+  },
+  container: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
   }
 })
