df90998c3e187ab7f0e93502f7f8a19b152ce143
Connie Chan <conniec321@gmail.com>
2022-07-07 22:40:33 -0400

    style: install libraries/#63

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Card, Provider ,Appbar ,Title,Paragraph , BottomNavigation} from 'react-native-paper';
 import HealthStat from './HealthStat';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
@@ -7,19 +8,22 @@ import SignUp from './createAccount/SignUp';
 import SetGoal from './createAccount/SetGoal';
 import DokiHome from './dokiHome/DokiHome';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
 
 
   return (
       <Tab.Navigator
-        tabBarOptions = {{
-          showLabel:false 
+        screenOptions = {{
+          showLabel:false,
+          tabBarStyle: { position: 'absolute' },
+          // headerShown: false,
+         
         }} 
       >
+        <Tab.Screen name="HOME" component={DokiHome}/>
         <Tab.Screen name="Backpack" component={SignUp}/>
-        <Tab.Screen name="Home" component={DokiHome}/>
         <Tab.Screen name="Health Data " component={HealthStat}/>
         <Tab.Screen name="Store" component={SetGoal}/>
       </Tab.Navigator>
