8cd5af17dbd5fa8175b8cf877b3e1b04ff74436d
Connie Chan <conniec321@gmail.com>
2022-07-08 01:53:32 -0400

    feat:api route for association/#31

--- server/api/user.js
+++ server/api/user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../db');
+const { User, Doki, User_Doki } = require('../db');
 const { requireToken } = require('./middleware.js');
 
 module.exports = router;
@@ -12,3 +12,34 @@ router.get('/', requireToken, async (req, res, next) => {
     next(err);
   }
 });
+
+router.post('/', async (req, res, next) => {
+  try {
+    //const user = await User.findByToken... when token available
+    const randomUser = await User.findByPk(1)
+  
+    const randomDoki = ["fox", "cat", "bunny"][Math.floor(Math.random() * 3)];
+    const doki = await Doki.findOne(
+      {where: {
+        type: randomDoki
+      }})
+    await randomUser.addDoki(doki)
+    let userDoki = await User_Doki.findOne(
+      {
+        where: {
+          userId: randomUser.id,
+          dokiId: doki.id
+        }
+      }
+    )
+    await userDoki.set(
+      {
+      dokiName: req.body.dokiName
+      }
+    )
+    console.log('USERDOKI', userDoki)
+    res.send(await userDoki.save())
+  } catch (err) {
+    next(err);
+  }
+})
