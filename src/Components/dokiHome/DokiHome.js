5919f55c9b4cbc0396ef12cc2a307be90b9d9f73
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-07-09 18:06:55 -0400

    wip: useUserDoki hook

--- src/Components/dokiHome/DokiHome.js
+++ src/Components/dokiHome/DokiHome.js
@@ -3,10 +3,15 @@ import { View } from 'react-native';
 import DokiEggView from './DokiEggView';
 import DokiView from './DokiView';
 import { useHatchProgress } from '../../hooks/useHatchProgress';
+import { useUserDokiData } from '../../hooks/useUserDokiData';
 
 const DokiHome = ({ navigation }) => {
   const hatchProgressData = useHatchProgress();
   const isEgg = hatchProgressData.hatchProgress < 1;
+
+  const userDoki = useUserDokiData();
+
+  console.log("USER DOKI", userDoki)
 
   return (
     <View>
