fffefeb844af745c4ad2aa92ba194b5a39cee1ec
Kristin Lam <klam623@gmail.com>
2022-07-09 16:46:56 -0400

    chore: fix merge conflict

--- src/Components/dokiHome/DokiView.js
+++ src/Components/dokiHome/DokiView.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Button } from 'react-native-paper';
-<<<<<<< HEAD
 import {
   StyledDokiHomeBackground,
   StyledDokiEggContainer,
@@ -10,31 +9,18 @@ import {
 import DokiProgressBar from './DokiProgressBar';
 import Doki from './Doki';
 import CountDisplay from './CountDisplay';
-import { useStepCount } from '../../Healthkit';
-=======
-import { StyledDokiHomeBackground, StyledDokiEggContainer, StyledOuterProgressBarContainer, StyledOuterCountersContainer } from '../styles';
-import DokiProgressBar from "./DokiProgressBar";
-import Doki from "./Doki";
-import CountDisplay from "./CountDisplay";
 import { useDailyStepCount } from '../../Healthkit';
->>>>>>> main
 import { useUserData } from '../../hooks/useUserData';
 
 const DokiView = () => {
   const [doki, setDoki] = useState({ type: 'bunny' });
 
-<<<<<<< HEAD
   const randomDoki = ['fox', 'cat', 'bunny'][Math.floor(Math.random() * 3)];
-  const stepCount = useStepCount();
-=======
-  const randomDoki = ["fox", "cat", "bunny"][Math.floor(Math.random() * 3)];
->>>>>>> main
 
   const stepCount = useDailyStepCount();
   const user = useUserData();
 
   return (
-<<<<<<< HEAD
     <StyledDokiHomeBackground
       source={require('../../../assets/backgrounds/dokihome_background.png')}
       resizeMode="cover"
@@ -47,32 +33,14 @@ const DokiView = () => {
         <CountDisplay
           counterType={'step'}
           count={stepCount}
-          goalCount={isLoading || isError ? 0 : user.dailyStepGoal}
+          goalCount={user && user.dailyStepGoal}
         />
-        <CountDisplay
-          counterType={'carrot'}
-          count={isLoading || isError ? 0 : user.carrotCount}
-        />
+        <CountDisplay counterType={'carrot'} count={user && user.carrotCount} />
       </StyledOuterCountersContainer>
       <StyledDokiEggContainer>
         <Doki doki={doki} />
       </StyledDokiEggContainer>
       <Button onPress={() => setDoki({ type: randomDoki })} mode="contained">
-=======
-    <StyledDokiHomeBackground source={require("../../../assets/dokihome_background.png")} resizeMode="cover">
-    <StyledOuterProgressBarContainer>
-      <DokiProgressBar name="Mood" progress={0.75}/>
-      <DokiProgressBar name="Hunger" progress={0.75}/>
-    </StyledOuterProgressBarContainer>
-    <StyledOuterCountersContainer>
-      <CountDisplay counterType={"step"} count={stepCount} goalCount={user && user.dailyStepGoal}/>
-      <CountDisplay counterType={"carrot"} count={user && user.carrotCount}/>
-    </StyledOuterCountersContainer>
-    <StyledDokiEggContainer>
-      <Doki doki={doki}/>
-    </StyledDokiEggContainer>
-      <Button onPress={() => setDoki({type: randomDoki}) } mode='contained'>
->>>>>>> main
         Change Doki
       </Button>
     </StyledDokiHomeBackground>
