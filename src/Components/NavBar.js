06b41ee57a076a85b6df296910ebac3fe1dd19d9
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-07-11 22:45:30 -0400

    style: navBar background image

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -21,10 +21,11 @@ const TabNavigator = () => {
               backgroundColor: '#000',
           }},
         tabBarShowLabel: false,
+        tabBarBackground: () => (<Image source={require('../../assets/backgrounds/navbar_background.png')}/>),
         tabBarStyle: [
           {
             display: "flex",
-            paddingTop:13,
+            paddingTop: 13,
             backgroundColor: '#59b2ff',
           },
           null
@@ -33,32 +34,32 @@ const TabNavigator = () => {
           switch (route.name) {
             case 'DokiHome':
               return (
-                <FontAwesome5 
-                  name={'home'} 
-                  style={{fontSize: 32}} 
+                <FontAwesome5
+                  name={'home'}
+                  style={{fontSize: 32}}
                   color='#333'/>
               )
             case 'User Settings':
               return (
-                <FontAwesome5 
-                  name={'shopping-bag'}   
+                <FontAwesome5
+                  name={'shopping-bag'}
                   style={{fontSize: 30}}
                   color='#333'
                 />
               )
             case 'HEALTH DATA':
               return (
-                <FontAwesome5 
-                name={'chart-bar'}   
-                style={{fontSize: 30}} 
+                <FontAwesome5
+                name={'chart-bar'}
+                style={{fontSize: 30}}
                 color='#333'
               />
               )
             case 'SetGoal':
               return (
-                <FontAwesome5 
-                name={'store'}   
-                style={{fontSize: 30}} 
+                <FontAwesome5
+                name={'store'}
+                style={{fontSize: 30}}
                 color='#333'
               />
               )
