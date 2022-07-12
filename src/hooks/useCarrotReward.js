54031151e8b3a17795e83c448bc99c4469c29982
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-07-12 13:01:47 -0400

    fix: refactor useCarrotReward to update on refresh

--- src/hooks/useCarrotReward.js
+++ src/hooks/useCarrotReward.js
@@ -4,12 +4,10 @@ import { useDailyStepCount } from "../Healthkit";
 import subDays from 'date-fns/subDays';
 
 export const useCarrotReward = (now) => {
-  console.log("NOW IN CARROT REWARD", now)
   const [carrotReward, setCarrotReward] = useState(null);
   const { user } = useUserData();
   const prevDay = subDays(new Date(now), 1).toISOString();
   const prevDaySteps = useDailyStepCount(prevDay);
-  console.log("PREV DAY STEPS", prevDaySteps)
 
   useEffect(() => {
     if (user && prevDaySteps >= 0) {
@@ -17,8 +15,6 @@ export const useCarrotReward = (now) => {
       setCarrotReward(Math.floor(prevDaySteps/dailyStepGoal*10)); // Carrot-StepCount Exchange Rate
     }
   }, [user, prevDaySteps, prevDay])
-
-  console.log("CARROT REWARD IN HOOK",  carrotReward)
 
   return carrotReward;
 };
