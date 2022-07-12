e16465daf9d0ae5c59b72fd586379b6f871fb1d4
Connie Chan <conniec321@gmail.com>
2022-07-12 11:26:04 -0400

    style: item in card component

--- src/Components/dokiPack/UserItem.js
+++ src/Components/dokiPack/UserItem.js
@@ -1,11 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 
 const UserItem = (props) => {
   console.log(props.name, 'inside pack')
   return (
     <View>
-      <Text>{props.name}</Text>
+      
+      <Card>
+        <Card.Cover source={require('../../../assets/items/ball.png')}/>
+        <Card.Content>
+          <Title>
+          {props.name}
+          </Title>
+          <Paragraph>
+            Quantity: {props.quantity}
+          </Paragraph>
+        </Card.Content>
+      </Card>
     </View>
   )
 }
