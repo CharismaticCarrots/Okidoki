dcca4f2c744be9f712d12dcf8b902bb5ef88d7a1
Kristin Lam <klam623@gmail.com>
2022-07-12 01:21:15 -0400

    feat: add in react query to get all items in store

--- src/Components/store/Store.js
+++ src/Components/store/Store.js
@@ -2,6 +2,32 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 import { StyledDokiHomeBackground, StyledHeading1 } from '../styles';
 import Item from './Item';
 
+import axios from 'axios';
+import { API_URL } from '../../../secrets.js';
+import { useQuery } from 'react-query';
+
+const fetchItemsData = async () => {
+  const { data } = await axios.get(`http://${API_URL}/api/items`);
+  return data;
+};
+
+export const useItemsData = () => {
+  const {
+    isLoading,
+    isError,
+    error,
+    data: items,
+  } = useQuery('items', fetchItemsData);
+
+  if (isLoading) {
+    console.log('LOADING');
+  } else if (isError) {
+    console.log('ERROR:', error);
+  } else {
+    return items;
+  }
+};
+
 const Store = () => {
   const storeItems = useItemsData();
   console.log('store Items:', storeItems);
