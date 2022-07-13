bc61d8aed3faad0aaa83084dcd8c9d5b770fce69
Connie Chan <conniec321@gmail.com>
2022-07-12 20:47:04 -0400

    feat:can purchase items

--- src/Components/store/Item.js
+++ src/Components/store/Item.js
@@ -1,5 +1,11 @@
-import { StyleSheet, View, Text, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Image , TouchableOpacity} from 'react-native';
+import axios from 'axios';
+import { API_URL } from '../../../secrets';
 import { Button } from 'react-native-paper';
+import { useMutation } from 'react-query';
+import { Popable, usePopable } from 'react-native-popable';
+import * as SecureStore from 'expo-secure-store';
 // import { StyledHeading1 } from '../styles';
 
 const imageNames = {
@@ -9,14 +15,85 @@ const imageNames = {
   'teddy bear': require('../../../assets/items/teddybear.png'),
 };
 
+
+
 const Item = (props) => {
+  const { ref, hide, show } = usePopable();
+  const [msgContent, setMsgContent] = useState(null);
+  const curCarrotCount = props.carrotCount
+  const price = props.price
+  const userMutation = useMutation(async (userUpdate) => {
+    const token = await SecureStore.getItemAsync('TOKEN');
+    if (token) {
+      const { data: updatedUser } = await axios.put(
+        `http://${API_URL}/api/user/`,
+        userUpdate,
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return updatedUser;
+    }
+  });
+
+  const userItemMutation = useMutation(async (itemId) => {
+    const token = await SecureStore.getItemAsync('TOKEN');
+    if (token) {
+      const { data: updatedUserItem } = await axios.post(
+        `http://${API_URL}/api/user/items/${itemId}`,
+        {},
+        {
+          headers: {
+            authorization: token,
+          },
+        }
+      );
+      return updatedUserItem;
+    }
+  })
+
+  const purchaseItem = (itemId) => {
+    if (curCarrotCount < price){
+      show();
+      setTimeout(() => hide(), 1000);
+      setMsgContent('UH OH, YOU\'RE DON\'T HAVE ENOUGH CARROTS!');
+    }
+    else {
+      const updatedCarrotCount = {
+        carrotCount: curCarrotCount - price
+      }
+      userMutation.mutate(updatedCarrotCount)
+      userItemMutation.mutate(itemId)
+      console.log(curCarrotCount)
+      show();
+      setTimeout(() => hide(), 1000);
+      setMsgContent(`${props.name.toUpperCase()} PURCHASED`);
+    }
+  }
+
+
   return (
+    <TouchableOpacity
+      onPress={() => purchaseItem(props.id)}
+    >
     <View style={styles.item}>
       <Image style={styles.itemImg} source={imageNames[props.name]} />
       <Text style={styles.itemName}>{props.name}</Text>
       <Text style={styles.itemPrice}>{props.price}</Text>
-      <Button mode='contained'>BUY</Button>
+      <Popable
+          ref={ref}
+          content={msgContent}
+          style={styles.popoverStyles}
+          animationType="spring"
+          caret={false}
+          backgroundColor='#59b2ff'
+          height={50}
+        >
+        </Popable>
     </View>
+    </TouchableOpacity>
   );
 };
 
@@ -48,4 +125,9 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: '#59b2ff',
   },
+  popoverStyles: {
+    alignSelf: "center",
+    marginTop: 500,
+    width: 200,
+  }
 });
--- src/Components/store/Store.js
+++ src/Components/store/Store.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
 import { StyledDokiHomeBackground, StyledHeading1 } from '../styles';
 import Item from './Item';
@@ -5,6 +6,7 @@ import CountDisplay from '../dokiHome/CountDisplay';
 import axios from 'axios';
 import { API_URL } from '../../../secrets.js';
 import { useQuery } from 'react-query';
+import { useUserData } from '../../hooks/useUserData';
 
 const fetchItemsData = async () => {
   const { data } = await axios.get(`http://${API_URL}/api/items`);
@@ -29,15 +31,25 @@ export const useItemsData = () => {
 };
 
 const Store = () => {
+  const { user } = useUserData();
   const storeItems = useItemsData();
   console.log('store Items:', storeItems);
+
+  const [curCarrotCount, setCurCarrotCount] = useState(0);
 
   let itemsList;
   if (storeItems) {
     itemsList = storeItems.map((item) => {
-      return <Item key={item.id} name={item.name} price={item.price} />;
+      return <Item key={item.id} name={item.name} price={item.price} id={item.id} carrotCount={curCarrotCount}/>;
     });
   }
+
+  useEffect(() => {
+    if (user) {
+      setCurCarrotCount(user.carrotCount);
+      console.log('USERRR', user)
+    }
+  }, [user]);
 
   return (
     <StyledDokiHomeBackground
@@ -48,7 +60,9 @@ const Store = () => {
         <StyledHeading1>Doki Mart</StyledHeading1>
         <View style={styles.items}>{itemsList}</View>
         <CountDisplay
+          style={{ marginTop: 400}}
           counterType={'carrot'}
+          count={curCarrotCount}
         />
       </View>
     </StyledDokiHomeBackground>
