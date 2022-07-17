354bcd3c59fed4b54706043b852a8afe091ef097
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-07-17 05:43:15 -0400

    feat: add 3 mood sprites for each doki

--- src/Components/dokiHome/Doki.js
+++ src/Components/dokiHome/Doki.js
@@ -18,30 +18,72 @@ const Doki = ({userDoki}) => {
     <StyledInnerDokiContainer>
       {userDoki.type === "fox" &&
         <Sprite
-          src={require('../../../assets/fox_crouch_strip8.png')}
-          totalSprites={8}
+          src={require('../../../assets/sprites/fox_sprites/fox_idle.png')}
+          totalSprites={24}
           tile={{ width: 60, height: 60 }}
           scale={5}
           framesPerSprite={8}
         />
+        // <Sprite
+        //   src={require('../../../assets/sprites/fox_sprites/fox_happy.png')}
+        //   totalSprites={8}
+        //   tile={{ width: 60, height: 60 }}
+        //   scale={5}
+        //   framesPerSprite={8}
+        // />
+        // <Sprite
+        // src={require('../../../assets/sprites/fox_sprites/fox_sleep.png')}
+        // totalSprites={5}
+        // tile={{ width: 60, height: 60 }}
+        // scale={5}
+        // framesPerSprite={25}
+        // />
       }
       {userDoki.type === "cat" &&
+        // <Sprite
+        //   src={require(`../../../assets/sprites/cat_sprites/cat_idle.png`)}
+        //   totalSprites={6}
+        //   tile={{ width: 128, height: 90 }}
+        //   scale={3}
+        //   framesPerSprite={15}
+        // />
+        // <Sprite
+        // src={require(`../../../assets/sprites/cat_sprites/cat_happy.png`)}
+        // totalSprites={18}
+        // tile={{ width: 128, height: 90 }}
+        // scale={2.5}
+        // framesPerSprite={15}
+        // />
         <Sprite
-          src={require('../../../assets/catSprites/cat.png')}
-          totalSprites={6}
-          tile={{ width: 128, height: 90 }}
-          scale={3}
-          framesPerSprite={15}
+        src={require(`../../../assets/sprites/cat_sprites/cat_sleep.png`)}
+        totalSprites={6}
+        tile={{ width: 128, height: 80 }}
+        scale={2.5}
+        framesPerSprite={15}
         />
       }
-      {userDoki.type === "bunny" &&
+      {userDoki.type === "whitefox" &&
         <Sprite
-          src={require('../../../assets/bunny_liedown_strip12.png')}
-          totalSprites={12}
-          tile={{ width: 40, height: 40 }}
-          scale={6}
-          framesPerSprite={15}
+          src={require('../../../assets/sprites/whitefox_sprites/whitefox_idle.png')}
+          totalSprites={24}
+          tile={{ width: 60, height: 60 }}
+          scale={5}
+          framesPerSprite={8}
         />
+        // <Sprite
+        //   src={require('../../../assets/sprites/whitefox_sprites/whitefox_happy.png')}
+        //   totalSprites={8}
+        //   tile={{ width: 60, height: 60 }}
+        //   scale={5}
+        //   framesPerSprite={8}
+        // />
+        // <Sprite
+        //   src={require('../../../assets/sprites/whitefox_sprites/whitefox_sleep.png')}
+        //   totalSprites={5}
+        //   tile={{ width: 60, height: 60 }}
+        //   scale={5}
+        //   framesPerSprite={25}
+        // />
       }
     </StyledInnerDokiContainer>
   );
