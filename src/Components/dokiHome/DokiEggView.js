69aa671995f1f8291aa06c6568b7fd57e082fa9c
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-07-21 15:37:58 -0400

    feat: add smoke sprite before hatch #319

--- src/Components/dokiHome/DokiEggView.js
+++ src/Components/dokiHome/DokiEggView.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, TouchableOpacity } from 'react-native';
 import { Popable, usePopable } from 'react-native-popable';
 import {
   StyledDokiHomeBackground,
@@ -11,6 +11,8 @@ import {
 import DokiProgressBar from './DokiProgressBar';
 import DokiEgg from './DokiEgg';
 import CountDisplay from './CountDisplay';
+import Sprite from '../Sprite';
+import images from '../../images';
 import { useQueryClient } from 'react-query';
 import { useHatchProgress } from '../../hooks/useHatchProgress';
 import { useUserDokiData } from '../../hooks/useUserDokiData';
@@ -24,6 +26,8 @@ const DokiEggView = ({now}) => {
   const { totalSteps, dailyStepGoal } = hatchProgressData;
   const { ref, hide, show } = usePopable();
   const [msgContent, setMsgContent] = useState(null);
+  const [isHatching, setIsHatching] = useState(null);
+  const isEggNow = hatchProgressData.hatchProgress < 1;
 
   return (
     <StyledDokiHomeBackground
@@ -52,24 +56,42 @@ const DokiEggView = ({now}) => {
           animationType="spring"
           backgroundColor="#59b2ff"
         ></Popable>
-        <TouchableOpacity onPress={hatchDokiEgg}>
+        {
+          isHatching ?
+          <View style={{marginTop: 100, marginBottom: 100}}>
+            <Sprite
+              src={images.sprites.egg.hatch}
+              totalSprites={6}
+              tile={{ width: 209, height: 208 }}
+              scale={1}
+              framesPerSprite={30}
+            />
+          </View>
+          :
+          <TouchableOpacity onPress={hatchDokiEgg}>
           <DokiEgg />
-        </TouchableOpacity>
+          </TouchableOpacity>
+        }
         <StyledDokiName>
           {userDokiData && userDokiData.user_doki.dokiName}
         </StyledDokiName>
       </StyledDokiEggContainer>
     </StyledDokiHomeBackground>
   );
+  function wait (timeout) {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
 
   function hatchDokiEgg () {
-    const isEggNow = hatchProgressData.hatchProgress < 1;
+    // const isEggNow = hatchProgressData.hatchProgress < 1;
     if (userDokiData.user_doki.isEgg && !isEggNow) {
       userDokiMutation.mutate({
         isEgg: false
       }, {
         onSuccess: () => {
-          queryClient.invalidateQueries(['userDoki']);
+          setIsHatching(true);
+          wait(2000).then(() =>
+          queryClient.invalidateQueries(['userDoki']));
         }
       });
     } else {
